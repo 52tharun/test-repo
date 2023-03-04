61d24f6d2b0a208f04495a92b9b9c905eb0d785d
52tharun <86329854+52tharun@users.noreply.github.com>
2023-03-04 18:25:37 +0000

    Create maintest.py

---
+++ maintest.py
@@ -0,0 +1,23 @@
+
+
+def quotient_remainder(a,b):
+    if b == 0:
+        raise ValueError ('B must be positive')
+        exit()
+    
+    myTuple = divmod(a,b)
+
+    if myTuple[1] < 0:
+      newTuple1 = myTuple[0] + 1
+      newTuple2 = myTuple[1] - b
+
+      newTuple = (newTuple1, newTuple2)
+      return newTuple
+  
+    return myTuple
+
+
+printvalue = quotient_remainder (-10,-3)
+
+print (printvalue)
+
