223b779fe405dd66427f026096b4098e54ed728a
52tharun <86329854+52tharun@users.noreply.github.com>
2023-03-12 20:22:53 +0000

    Update maintest.py

--- maintest.py
+++ maintest.py
@@ -2,19 +2,19 @@
 
 def quotient_remainder(a,b):
     if b == 0:
-        raise ValueError ('B must be positive')
-        exit()
+        raise ValueError('B must be positive')
     
     myTuple = divmod(a,b)
 
     if myTuple[1] < 0:
-      newTuple1 = myTuple[0] + 1
-      newTuple2 = myTuple[1] - b
+        newTuple1 = myTuple[0] + 1
+        newTuple2 = myTuple[1] - b
 
-      newTuple = (newTuple1, newTuple2)
-      return newTuple
-  
+        newTuple = (newTuple1, newTuple2)
+        return newTuple
+
     return myTuple
+
 
 
 assert quotient_remainder(7, 3) == (2, 1)
